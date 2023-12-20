46b6db13ebc286690199b6fd5585d91ce2990a81
Taichi Araki <araki@fun.bio.keio.ac.jp>
2023-12-20 17:53:32 +0900

    EventHandler(ActionListener) should be written with anonymous lambda function.

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 
-public class Main extends Application implements EventHandler<ActionEvent>{
+public class Main extends Application {
     private Button button;
     public static void main(String[] args) {
         launch(args);
@@ -18,15 +19,9 @@ public class Main extends Application implements EventHandler<ActionEvent>{
         button = new Button();
         button.setText("Click me");
 
-//        This way would be more advanced.
-//        button.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent actionEvent) {
-//                System.out.println("Button clicked.");
-//            }
-//        });
-
-        button.setOnAction(this);
+        button.setOnAction(e -> {
+            System.out.println("EventHandler(ActionListener) should be written with anonymous lambda function.");
+        });
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
@@ -36,10 +31,5 @@ public class Main extends Application implements EventHandler<ActionEvent>{
         stage.show();
     }
 
-    @Override
-    public void handle(ActionEvent actionEvent) {
-        if(actionEvent.getSource() == button) {
-            System.out.println("From handler function.");
-        }
-    }
+
 }
